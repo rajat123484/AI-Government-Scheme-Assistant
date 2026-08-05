e5d4eb8d9369a1fc6522ae2076297e12c5f68f64
Shaurya Sharma <shauryabest1@gmail.com>
2026-08-05 15:14:07 +0530

    Connect frontend to Railway backend

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -6,7 +6,7 @@
 // IMPORTANT:
 // Change this if your FastAPI backend runs on another port.
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "ai-government-scheme-assistant-production.up.railway.app";
 
 
 // ============================================================
